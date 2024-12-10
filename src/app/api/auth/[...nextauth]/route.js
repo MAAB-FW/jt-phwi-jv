ea3698e30c0e24fbfe2cc341b90629f085f47b64
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-12-10 11:12:15 +0600

    add: nextauth setup and necessary packages installed

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,52 @@
+import { compare } from "bcrypt";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectDB } from "../../../../lib/connectDB";
+
+const handler = NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+    maxAge: 60 * 60 * 24 * 30,
+  },
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        try {
+          const { email, password } = credentials;
+          if (!email || !password) {
+            return null;
+          }
+          const db = await connectDB();
+          const user = await db.collection("users").findOne({ email });
+          if (!user) {
+            throw new Error("Invalid credentials");
+          }
+          const isPasswordValid = await compare(password, user.password);
+          if (!isPasswordValid) {
+            throw new Error("Invalid credentials");
+          }
+          return {
+            id: user._id.toString(),
+            email: user.email,
+            name: user.name,
+            image: user.image,
+            role: user.role,
+          };
+        } catch (error) {
+          console.log(error);
+          throw new Error(error.message);
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+});
+
+export { handler as GET, handler as POST };
--- src/app/api/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export const GET = async () => {
-  try {
-    return NextResponse.json("working");
-  } catch (error) {
-    console.log(error);
-  }
-};
