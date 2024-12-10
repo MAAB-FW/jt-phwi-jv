8363884c5d87eaf5a83b58be15d477c85f08da4a
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-12-10 16:35:30 +0600

    update: middleware updated for conditional redirecting

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,13 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
+import { getUserRole } from "./services/getData";
 
 const secret = process.env.NEXTAUTH_SECRET;
 
 export const middleware = async (request: NextRequest) => {
   const token = await getToken({ req: request, secret });
   const pathname = request.nextUrl.pathname;
+
   if (
     pathname.startsWith("/login") ||
     pathname.startsWith("/register") ||
@@ -17,5 +19,15 @@ export const middleware = async (request: NextRequest) => {
   if (!token) {
     return NextResponse.redirect(`${process.env.NEXT_PUBLIC_API_URL}/login`);
   }
+  if (token.email) {
+    const { role } = await getUserRole(token.email);
+    if (role === "admin" && (pathname === "/" || pathname === "/lessons")) {
+      return NextResponse.redirect("/dashboard");
+    }
+    if (role === "user" && (pathname === "/dashboard" || pathname === "/")) {
+      return NextResponse.redirect("/lessons");
+    }
+  }
+
   return NextResponse.next();
 };
