b154277fd5f464950a1f79d5f404350106d8504a
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-12-11 16:11:46 +0600

    add: add-lesson post calling api function created

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -1,9 +1,24 @@
+import { LessonFormData } from "@/app/dashboard/add-lessons/page";
 import axios from "axios";
 
 export const getUserInfo = async (email: string) => {
   try {
     const res = await axios(
       `${process.env.NEXT_PUBLIC_API_URL}/api/user-info/${email}`
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return {};
+  }
+};
+
+// add lesson post func
+export const addLesson = async (data: LessonFormData) => {
+  try {
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/admin/add-lessons`,
+      data
     );
     return res.data;
   } catch (error) {
