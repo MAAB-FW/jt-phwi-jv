612c6fb188aeb209e69e3afc7fc843e57b397eeb
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-12-11 23:22:56 +0600

    add: add-vucabulary post api created

---
+++ src/app/api/admin/add-vucabulary/route.ts
@@ -0,0 +1,65 @@
+import { VucabularyFormData } from "@/app/dashboard/add-vocabularies/page";
+import { connectDB } from "@/lib/connectDB";
+import { NextResponse } from "next/server";
+
+export const POST = async (req: Request) => {
+  const db = await connectDB();
+  const {
+    word,
+    meaning,
+    pronunciation,
+    whenToSay,
+    lessonNo,
+    adminMail,
+    example,
+  }: VucabularyFormData = await req.json();
+  const vucabularyCollection = db.collection("vucabulary");
+
+  if (
+    !lessonNo ||
+    !word ||
+    !meaning ||
+    !pronunciation ||
+    !whenToSay ||
+    !example ||
+    !adminMail
+  ) {
+    return NextResponse.json({
+      message: "All fields are required!",
+      status: 400,
+    });
+  }
+
+  if (typeof lessonNo !== "number") {
+    return NextResponse.json({
+      message: "Lesson number must be a number!",
+      status: 400,
+    });
+  }
+
+  const isExist = await vucabularyCollection.findOne({
+    $or: [{ word }, { lessonNo }],
+  });
+  if (isExist) {
+    return NextResponse.json({
+      message: "Vucabulary already exists!",
+      status: 400,
+    });
+  }
+
+  try {
+    const res = await vucabularyCollection.insertOne({
+      word,
+      meaning,
+      pronunciation,
+      whenToSay,
+      lessonNo,
+      adminMail,
+      example,
+    });
+    return NextResponse.json(res);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Something went wrong!", status: 500 });
+  }
+};
