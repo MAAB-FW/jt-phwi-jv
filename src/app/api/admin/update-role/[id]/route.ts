8a0d39451daf9471ec5e8ef31bb0077775026eec
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-12-11 21:46:28 +0600

    add: update user role api created

---
+++ src/app/api/admin/update-role/[id]/route.ts
@@ -0,0 +1,45 @@
+import { connectDB } from "@/lib/connectDB";
+import { ObjectId } from "mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+export const PATCH = async (
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) => {
+  const db = await connectDB();
+  const { role }: { role: "admin" | "user" } = await req.json();
+  const _id = (await params).id;
+  console.log("🚀 ~ _id:", _id)
+
+  const usersCollection = db.collection("users");
+  if (role !== "admin" && role !== "user") {
+    return NextResponse.json({
+      message: "Role must be either admin or user!",
+      status: 400,
+    });
+  }
+
+  try {
+    const isExist = await usersCollection.findOne({ _id: new ObjectId(_id) });
+    if (!isExist) {
+      return NextResponse.json({
+        message: "User doesn't exists!",
+        status: 400,
+      });
+    }
+
+    const res = await usersCollection.updateOne(
+      { _id: new ObjectId(_id) },
+      {
+        $set: {
+          role,
+        },
+      }
+    );
+    console.log("🚀 ~ res:", res);
+    return NextResponse.json(res);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Something went wrong!", status: 500 });
+  }
+};
