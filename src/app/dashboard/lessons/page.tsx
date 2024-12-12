86ea6f332608ac68f9ee9463042327e91b49a3a9
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-12-12 10:54:08 +0600

    add: dashboard lessons page created and implemented

--- src/app/dashboard/lessons/page.tsx
+++ src/app/dashboard/lessons/page.tsx
@@ -1,11 +1,83 @@
-import React from 'react';
+"use client";
 
-const Page = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { getLessons, getVocabularyCountALesson } from "@/services/getData";
+import { useQuery } from "@tanstack/react-query";
+
+interface Lesson {
+  _id: string;
+  name: string;
+  lessonNo: number;
 }
 
-export default Page;
+const LessonsPage = () => {
+  const { data: lessons = [], isLoading } = useQuery<Lesson[]>({
+    queryKey: ["get-lessons"],
+    queryFn: async () => {
+      const { lessons } = await getLessons();
+      return lessons;
+    },
+  });
+
+  return (
+    <div className="p-6">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">
+            Lessons Management
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          {isLoading ? (
+            <div className="flex h-48 items-center justify-center">
+              <span className="loading loading-spinner loading-lg"></span>
+            </div>
+          ) : (
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Lesson Number</TableHead>
+                  <TableHead>Lesson Name</TableHead>
+                  <TableHead>Vocabulary Count</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {lessons.map((lesson) => (
+                  <TableData key={lesson._id} lesson={lesson}></TableData>
+                ))}
+              </TableBody>
+            </Table>
+          )}
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default LessonsPage;
+
+const TableData = ({ lesson }: { lesson: Lesson }) => {
+  const { data: vocabularyCount = 0 } = useQuery({
+    queryKey: ["get-vocabulary-count", lesson.lessonNo],
+    queryFn: async () => {
+      const { count } = await getVocabularyCountALesson(lesson.lessonNo);
+      return count;
+    },
+  });
+
+  return (
+    <TableRow>
+      <TableCell>{lesson.lessonNo}</TableCell>
+      <TableCell>{lesson.name}</TableCell>
+      <TableCell>{vocabularyCount}</TableCell>
+    </TableRow>
+  );
+};
