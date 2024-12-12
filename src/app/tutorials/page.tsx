92d7b15b70f13259e7e3252c3e593bf848a315c2
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-12-12 18:39:27 +0600

    update: tutorials page's data are now dynamic

--- src/app/tutorials/page.tsx
+++ src/app/tutorials/page.tsx
@@ -1,63 +1,74 @@
 "use client";
 
+import { getAllTutorials } from "@/services/getData";
+import { useQuery } from "@tanstack/react-query";
+
 interface Tutorial {
-  id: number;
+  _id: string;
   title: string;
   videoId: string;
 }
 
-const tutorials: Tutorial[] = [
-  {
-    id: 1,
-    title: "Japanese Alphabet - Hiragana Made Easy",
-    videoId: "wD3FJgij79c",
-  },
-  {
-    id: 2,
-    title: "Learning Japanese is Easy... Here's How",
-    videoId: "T09w34WME5Q",
-  },
-  {
-    id: 3,
-    title: "How I became fluent in Japanese | Kanji",
-    videoId: "4CvoPRZsejc",
-  },
-  {
-    id: 4,
-    title: "Watch this before studying Japanese",
-    videoId: "4MNnv5hsfYw",
-  },
-  {
-    id: 5,
-    title: "Is Duolingo Really a Good Way to Study Japanese?",
-    videoId: "j1fNAA8mdFY",
-  },
-  {
-    id: 6,
-    title: "10 JAPANESE Phrases Every Traveler Should Know (Basic Japanese)",
-    videoId: "UJR5rm9HhTA",
-  },
-  {
-    id: 7,
-    title:
-      "150 basic Japanese phrases: You can learn it completely in 20 minutes. #learnjapanese #beginners",
-    videoId: "G_oC7anVuA8",
-  },
-  {
-    id: 8,
-    title: "Basic Kanji Characters",
-    videoId: "mPppVDX_GiY",
-  },
-];
+// const tutorials: Tutorial[] = [
+//   {
+//     id: 1,
+//     title: "Japanese Alphabet - Hiragana Made Easy",
+//     videoId: "wD3FJgij79c",
+//   },
+//   {
+//     id: 2,
+//     title: "Learning Japanese is Easy... Here's How",
+//     videoId: "T09w34WME5Q",
+//   },
+//   {
+//     id: 3,
+//     title: "How I became fluent in Japanese | Kanji",
+//     videoId: "4CvoPRZsejc",
+//   },
+//   {
+//     id: 4,
+//     title: "Watch this before studying Japanese",
+//     videoId: "4MNnv5hsfYw",
+//   },
+//   {
+//     id: 5,
+//     title: "Is Duolingo Really a Good Way to Study Japanese?",
+//     videoId: "j1fNAA8mdFY",
+//   },
+//   {
+//     id: 6,
+//     title: "10 JAPANESE Phrases Every Traveler Should Know (Basic Japanese)",
+//     videoId: "UJR5rm9HhTA",
+//   },
+//   {
+//     id: 7,
+//     title:
+//       "150 basic Japanese phrases: You can learn it completely in 20 minutes. #learnjapanese #beginners",
+//     videoId: "G_oC7anVuA8",
+//   },
+//   {
+//     id: 8,
+//     title: "Lesson 1. Learn Japanese in Urdu and Hindi",
+//     videoId: "IhUeEh10Xfs",
+//   },
+// ];
 
 export default function TutorialsPage() {
+  const { data: tutorials = [] } = useQuery<Tutorial[]>({
+    queryKey: ["tutorials"],
+    queryFn: async () => {
+      const { tutorials } = await getAllTutorials();
+      return tutorials;
+    },
+  });
+
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="mb-8 text-3xl font-bold">Japanese Language Tutorials</h1>
       <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
-        {tutorials.map((tutorial) => (
+        {tutorials?.map((tutorial) => (
           <div
-            key={tutorial.id}
+            key={tutorial._id}
             className="overflow-hidden rounded-lg bg-white shadow-lg"
           >
             <div className="relative h-0 pb-[56.25%]">
