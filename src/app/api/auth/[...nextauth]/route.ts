4788218e891aa2f957898676c2028988e08d513b
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-12-16 16:28:33 +0600

    update: typescript implemented for connectDB and nextauth file.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,14 @@
 import { compare } from "bcrypt";
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions, User } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { connectDB } from "../../../../lib/connectDB";
 
-const handler = NextAuth({
+interface Credentials {
+  email: string;
+  password: string;
+}
+
+const authOptions: AuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
@@ -15,12 +20,14 @@ const handler = NextAuth({
         email: {},
         password: {},
       },
-      async authorize(credentials) {
+      async authorize(
+        credentials: Credentials | undefined
+      ): Promise<User | null> {
         try {
-          const { email, password } = credentials;
-          if (!email || !password) {
+          if (!credentials?.email || !credentials?.password) {
             return null;
           }
+          const { email, password } = credentials;
           const db = await connectDB();
           const user = await db.collection("users").findOne({ email });
           if (!user) {
@@ -31,13 +38,14 @@ const handler = NextAuth({
             throw new Error("Invalid credentials");
           }
           return {
+            id: user._id.toString(),
             email: user.email,
             name: user.name,
             image: user.photoUrl,
           };
         } catch (error) {
           console.log(error);
-          throw new Error(error.message);
+          throw new Error((error as Error).message);
         }
       },
     }),
@@ -45,6 +53,8 @@ const handler = NextAuth({
   pages: {
     signIn: "/login",
   },
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -1,12 +1,12 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { Db, MongoClient, ServerApiVersion } from "mongodb";
 
-let db;
+let db: Db;
 
-export const connectDB = async () => {
+export const connectDB = async (): Promise<Db> => {
   if (db) return db;
   try {
     const uri = process.env.MONGO_URI;
-    const client = new MongoClient(uri, {
+    const client = new MongoClient(uri!, {
       serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
@@ -17,5 +17,6 @@ export const connectDB = async () => {
     return db;
   } catch (error) {
     console.log(error);
+    throw new Error("Failed to connect to database");
   }
 };
